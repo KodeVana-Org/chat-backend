70dfc9bd04a57b7ca97fe620a95a6b327d75f4b2
usernamedinesh <borod9200@gmail.com>
2025-02-03 00:55:53 +0530

    all user: get all user except self

--- src/controllers/user/getAllUser.ts
+++ src/controllers/user/getAllUser.ts
@@ -5,19 +5,20 @@ import { ApiError } from "../../utils/ApiError";
 import { ApiResponse } from "../../utils/ApiResponse";
 
 const getAllUser = asyncHandler(async (req: Request, res: Response) => {
-  try {
-    const allUser = await User.find({});
-    if (allUser.length === 0) {
-      throw new ApiError(404, "User now found");
+    try {
+        const { userId } = req.body;
+        const allUser = await User.find({ _id: { $ne: userId } }); // Exclude userId from results
+        if (allUser.length === 0) {
+            throw new ApiError(404, "User now found");
+        }
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, { user: allUser }, "successfully fetch all user"),
+            );
+    } catch (error) {
+        throw new ApiError(500, "Error while fetching users");
     }
-    return res
-      .status(200)
-      .json(
-        new ApiResponse(200, { user: allUser }, "successfully fetch all user"),
-      );
-  } catch (error) {
-    throw new ApiError(500, "Error while fetching users");
-  }
 });
 
 export { getAllUser };
