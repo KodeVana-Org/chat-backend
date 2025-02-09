9ca0746af835e3904f0081957fc26204bd0c4527
usernamedinesh <borod9200@gmail.com>
2025-02-09 05:46:31 +0530

    multer configuration added

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,9 +1,28 @@
 import multer from "multer";
 
-const multerUpload = multer({
-  limits: {
-    fileSize: 1024 * 1024 * 5,
-  },
+
+//const storage = multer.diskStorage({
+//    destination: (req, file, cb) => {
+//        cb(null, "./src/uploads/")
+//    },
+//    filename: (req, file, cb) => {
+//        cb(null, `${Date.now()}-${file.originalname}`)
+//    },
+//});
+
+//here for storing media in uplaod for temperorary
+//
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "../uploads")); // Save files to the "uploads" folder
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`); // Unique filename
+    },
 });
 
-export default multerUpload;
+
+// create multer instance
+const upload = multer({ storage })
+
+export { upload }
