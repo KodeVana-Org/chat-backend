ce59bc7ade9a9961099ee18d310a76342f4bedc1
usernamedinesh <borod9200@gmail.com>
2025-02-10 00:45:07 +0530

    add validation in index.js

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import env from "./config/dotenvConfig";
+import mongoose from "mongoose";
 import { User } from "./models/user.Model";
 import { connectToDatabase } from "./config/db";
 import { httpServer } from "./app";
@@ -12,15 +13,26 @@ const initSocket = () => {
     io.on("connection", async (socket) => {
         console.log(`User connected: ${socket.id}`);
 
-        const userId = socket.handshake.auth?.userId || socket.handshake.query?.userId;
-        console.log("I got userID from handshake: ", userId)
+        //const userId = socket.handshake.auth?.userId || socket.handshake.query?.userId;
+        const userId = socket.handshake.query?.userId;
+        console.log("I got userID from query handshake: ", userId)
 
-        if (!userId) {
-            console.log("User Id is missing in handshake")
-            return
+        // Validate userId
+        if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
+            console.log("Invalid or missing User Id in query");
+            socket.disconnect(true);
+            return;
         }
+
         // Set user status to online
-        await User.findByIdAndUpdate(userId, { status: "online" });
+        try {
+            await User.findByIdAndUpdate(userId, { status: "online" });
+            io.emit("updateUserStatus", { userId, status: "online" });
+        } catch (error) {
+            console.error("Error updating user status to online:", error);
+            socket.disconnect(true);
+            return;
+        }
 
         // Notify everyone that this user is online
         io.emit("updateUserStatus", { userId, status: "online" });
