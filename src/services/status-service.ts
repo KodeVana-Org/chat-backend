5517ac1307317523bdb6a6c0fb0422877fe87341
Sahilsway <sahilswargiary.personal@gmail.com>
2025-03-05 12:16:56 +0530

    Latest backend

--- src/services/status-service.ts
+++
@@ -1,7 +0,0 @@
-import { redisClient } from "../config/redisConfig"
-
-const publishUserStatus = async (userId: string, status: "online" | "offline") => {
-    await redisClient.publish("user-status", JSON.stringify({ userId, status }))
-}
-
-export { publishUserStatus }
